91771accfbae8e61dea5c502d3f7369c67263b10
trainee.qodor <trainee.qodors@gmail.com>
2026-03-09 17:33:21 +0530

    Revert "Backend Setup"
    This reverts commit f9ade6997fba5d7991a5f9c63293ebc27579590a.

--- src/routes/passwordReset.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { resetPassword, forgotPassword } = require("../controllers/passwordReset.controller");
-const router = express.Router();
-
-
-router.post("/resetPassword" , forgotPassword);
-router.post("/resetPassword/:token" , resetPassword);
-
-module.exports = router
