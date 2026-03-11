8c4cb449296ed87f228f573a1c5507e782c5cc81
mayur-jp <mayurj.pambhar@gmail.com>
2026-03-11 18:12:30 +0530

    mohit branch setup

--- server.js
+++ server.js
@@ -1,51 +1,48 @@
-
-const express = require('express')
-const orgRouter =  require('./src/routes/org.routes.js')
-const  errorHandler = require("./src/middlewares/error.middleware.js")
-const cors = require("cors")
+const express = require("express");
+const orgRouter = require("./src/routes/org.routes.js");
+const errorHandler = require("./src/middlewares/error.middleware.js");
+const cors = require("cors");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const passwordResetRouter = require("./src/routes/passwordReset.routes.js");
-const tenantRouter = require("./src/routes/tenant.routes.js")
-const documentRouter = require("./src/routes/document.routes.js")
-const orgDetailsRouter = require("./src/routes/orgDetails.routes.js")
-
+const tenantRouter = require("./src/routes/tenant.routes.js");
+const documentRouter = require("./src/routes/document.routes.js");
+const orgDetailsRouter = require("./src/routes/orgDetails.routes.js");
 
 const PORT = process.env.PORT;
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // Local dev
+      "http://192.168.0.40:5173", // Your local IP for other PCs
+      "http://192.168.0.37:5173",
+      "http://192.168.0.35:5173",
+      // Live site
+    ],
+    credentials: true,
+  }),
+);
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",          // Local dev
-    "http://192.168.0.40:5173",       // Your local IP for other PCs
-    "http://192.168.0.37:5173" // Live site
-  ],
-  credentials: true
-}));
-
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 
 app.use(express.json());
-app.use(cookieParser())
-app.use(express.urlencoded({extended : true}))
- app.use('/public', express.static('public'))
- 
-app.get("/", (req,res) => {
-  res.send("hello World")
-})
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static("public"));
 
-app.use("/api/org" , orgRouter )
-app.use("/api/password" , passwordResetRouter)
-app.use("/api/tenant" , tenantRouter )
-app.use("/api/document" , documentRouter)
-app.use("/api/orgDetails" , orgDetailsRouter)
+app.get("/", (req, res) => {
+  res.send("hello World");
+});
 
+app.use("/api/org", orgRouter);
+app.use("/api/password", passwordResetRouter);
+app.use("/api/tenant", tenantRouter);
+app.use("/api/document", documentRouter);
+app.use("/api/orgDetails", orgDetailsRouter);
 
 app.use(errorHandler);
 
-
-
-
-app.listen(PORT , "0.0.0.0" ,() => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running at :${PORT}`);
 });
